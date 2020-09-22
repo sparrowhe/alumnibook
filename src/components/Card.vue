5012d592069063724913cbfb8eb42473613ffdf6
SparrowHe <1441373096@qq.com>
2020-09-22 13:32:56 +0800

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="card">
+  <div class="card-content">
+    <h5 class="card-title">{{ name }}</h5>
+      <div class="card-body" style="text-align:left">
+        {{ content }}
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    name: String,
+    content: String
+  }
+}
+</script>
+
+
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="navbar">
+      <div class="container">
+        <div class="navbar-logo">
+          <h4 class="heading">Alumnibook</h4>
+        </div>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: "aheader"
+}
+</script>
+
+<style>
+
+</style>
